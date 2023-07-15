12572c27aabb3d95b94590439e9a94a9bc9133a4
ms-msbhh-ms <shohag.i.sajjad@gmail.com>
2023-07-15 22:32:31 +0600

    react icon

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactIcon from "./components/reactIcon";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <span className="text-[50px]">
+          <ReactIcon />
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ReactIcon.jsx
@@ -0,0 +1,10 @@
+import { FaWhatsapp } from "react-icons/fa";
+function ReactIcon() {
+  return (
+    <div>
+      <FaWhatsapp />
+    </div>
+  );
+}
+
+export default ReactIcon;
